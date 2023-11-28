f98bc1f057983676e9a048e648d71176d4392c22
Mukesh Kushwaha <99179878+RAKESHKUSHWAHA7518@users.noreply.github.com>
2023-11-28 18:22:22 +0530

    episode-11

---
+++ src/components/ItemList.js
@@ -0,0 +1,47 @@
+
+ import { CDN_URL, Menu_API } from "../utils/constants";
+const ItemList= ({items})=>{
+    console.log(items)
+    return( <div> 
+
+
+        
+       {items.map((item) => (
+      <div className="p-2 m-2  border-black border-b-2 text-left flex justify-between " key={item.card.info.id}> 
+      
+      
+      <div className="w-9/12">
+        <div  className="py-2">
+        <span>{item.card.info.name}</span>  
+         
+         <span>{"      "} -₹{"      "} {item.card.info.price/100 ||item.card.info.defaultPrice/100  }Rs</span> 
+          
+        </div>
+      <p className=" text-xs">{item.card.info.description}</p><br></br>
+      </div>
+      
+      <div className="w-3/12 p-4">
+
+      
+      <div className="absolute">
+      
+      <button className="p-2 mx-10 rounded-lg bg-white shadow-lg  m-auto">Add +</button>
+      </div>
+
+      <img  className="bg-green-100 rounded-md "src={ Menu_API +item.card.info.imageId } /> 
+      </div>
+
+      </div>
+      
+      ) )} 
+     
+    
+    </div> );
+      
+ 
+};
+
+export default ItemList;
+
+
+//  src={ Menu_API +item.card.info.imageId }
